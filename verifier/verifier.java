d4f85b609a804171cb4a37a5bddcc941e3a878fd
DuncanMilne <duncan.milne1@gmail.com>
2017-11-06 20:57:38 +0000

    first commit including basic java file for the verifier

---
+++ verifier/verifier.java
@@ -0,0 +1,10 @@
+public class Verifier {
+
+	// Takes two arguments, firstly a file containing the maze configuration in X format, secondly, the score
+	// produced by the IP model
+	public static void main(String[] args) {
+
+
+	}
+
+}
